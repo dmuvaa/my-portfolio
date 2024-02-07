f94e8b36577f5c8f42ee888fd601d717cf66cfab
Dennis Muvaa <dmuvaa70@gmail.com>
2024-02-07 15:56:39 +0300

    Create Login Logic

--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,25 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, current_user, login_required
+from .models import User, db
+from . import bcrypt
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check if username and password are correct')
+    
     return render_template('login.html')
-
-@auth.route('/logout')
-def logout():
-    return redirect(url_for('main.home'))
-
-@auth.route('/register')
-def register():
-    return render_template('register.html')
