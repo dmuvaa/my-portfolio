3820cddbeadb60a85a1453a475f805824758e34d
HassanMunene <awanzihassan@gmail.com>
2024-02-10 15:43:09 +0300

    completed the intro part responsiveness

--- portfolio/src/components/Intro/Intro.js
+++ portfolio/src/components/Intro/Intro.js
@@ -6,24 +6,26 @@ import { IoMdBriefcase } from "react-icons/io";
 const IntroSection = () => {
     return (
         <section id='intro' className='introSection'>
-            <div className='intoContent'>
-                <span className='hello'>Hello,</span>
-                <span className='introText'>
-                    I'm <span className='introName'>Dennis</span>
-                    <br/>Software Engineer
-                </span>
-                <p className='introParagraph'>
-                    I am a skilled software engineer with exprience in creating robust 
-                    and user friendly applications
-                </p>
-                <Link>
-                    <button className='btn'>
-                        <IoMdBriefcase/>
-                        Hire me
-                    </button>
-                </Link>
+            <div className='container'>
+                <div className='intoContent'>
+                    <span className='hello'>Hello,</span>
+                    <span className='introText'>
+                        I'm <span className='introName'>Dennis</span>
+                        <br/>Software Engineer
+                    </span>
+                    <p className='introParagraph'>
+                        I am a skilled software engineer with exprience in creating <br/>robust 
+                        and user friendly applications
+                    </p>
+                    <Link>
+                        <button className='btn'>
+                            <IoMdBriefcase className='btnImg'/>
+                            <span>Hire me</span>
+                        </button>
+                    </Link>
+                </div>
+                <img src={introImg} alt='profile' className='introImg'></img>
             </div>
-            <img src={introImg} alt='profile' className='introImg'></img>
         </section>
     )
 }
