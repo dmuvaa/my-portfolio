50eb87ed464db310a682bd1cd68c75d6022a4c9d
HassanMunene <awanzihassan@gmail.com>
2024-02-10 12:36:05 +0300

    added the navigation component

--- portfolio/src/components/Navbar/navbar.js
+++ portfolio/src/components/Navbar/navbar.js
@@ -1,9 +1,23 @@
 import React from "react";
 import './navbar.css';
+import { FiMessageSquare } from "react-icons/fi";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="navbar">
+            <h1 className="logo">DM<span>.</span></h1>
+            <div className="desktopMenu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Clients</Link>
+            </div>
+            <button className="desktopMenuBtn">
+                <FiMessageSquare className="messageBtn"/>
+                <span>Contact Me</span>
+            </button>
+        </nav>
     )
 }
 
