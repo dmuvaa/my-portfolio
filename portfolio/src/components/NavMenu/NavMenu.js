c44fa641726c0f5ef7d55c14aea66cac772ce52d
HassanMunene <awanzihassan@gmail.com>
2024-02-11 14:54:09 +0300

    completed navigation bar responsiveness

--- portfolio/src/components/NavMenu/NavMenu.js
+++ portfolio/src/components/NavMenu/NavMenu.js
@@ -1,15 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { MdMenu, MdClose } from "react-icons/md";
 import './navMenu.css'
 
 const NavMenu = () => {
+    const [showNav, setShowNav] = useState(false);
+    //console.log(showNav);
     return (
         <div className="navMenu">
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/about">About</NavLink></li>
-                <li><NavLink to="/projects">Projects</NavLink></li>
-                <li><NavLink to="/contact">Contact</NavLink></li>
+            <div 
+                className="mobileMenuIcon" 
+                onClick={() => setShowNav(!showNav)} 
+                role="button" 
+                onKeyDown={() => setShowNav(!showNav)} 
+                tabIndex={0}
+            >
+                <MdMenu />
+            </div>
+            <ul className={!showNav ? 'navItems hide-navItems' : 'navItems'}>
+                <div 
+                    className="closeNavIcon"
+                    onClick={() => setShowNav(!showNav)} 
+                    role="button" 
+                    onKeyDown={() => setShowNav(!showNav)} 
+                    tabIndex={0}
+                >
+                    <MdClose/>
+                </div>
+                <li>
+                    <NavLink 
+                        to="/"
+                        onClick={() => setShowNav(!showNav)} 
+                        role="button" 
+                        onKeyDown={() => setShowNav(!showNav)} 
+                        tabIndex={0}  
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/about" 
+                        onClick={() => setShowNav(!showNav)} 
+                        role="button" 
+                        onKeyDown={() => setShowNav(!showNav)} 
+                        tabIndex={0}
+                    >
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/projects" 
+                        onClick={() => setShowNav(!showNav)} 
+                        role="button" 
+                        onKeyDown={() => setShowNav(!showNav)} 
+                        tabIndex={0}
+                    >
+                        Projects
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/contact" 
+                        onClick={() => setShowNav(!showNav)} 
+                        role="button" 
+                        onKeyDown={() => setShowNav(!showNav)} 
+                        tabIndex={0}
+                    >
+                        Contact
+                    </NavLink>
+                </li>
             </ul>
         </div>
     )
