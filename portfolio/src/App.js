3f35faa70eb3976db2c30654716612abe8437eb8
HassanMunene <awanzihassan@gmail.com>
2024-02-11 12:25:30 +0300

    linked the pages using react-router dom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,21 @@
 import NavMenu from "./components/NavMenu";
-import { BrowserRouter as BRouter } from 'react-router-dom'
+import { BrowserRouter as BRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <>
       <BRouter>
         <NavMenu/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
       </BRouter>
     </>
   )
